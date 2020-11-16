7cf461abb1ff45cbed0c51756074a8939fbd2d60
herry081288 <herry081288@gmail.com>
2020-11-16 09:38:49 +0700

    connection

---
+++ config/connect.php
@@ -0,0 +1,9 @@
+<?php 
+//ini untuk mengkoneksikan ke database
+// $server = "localhost";
+// $user = "root";
+// $passw = "";
+// $db = "db_simpeg";
+ $conn = mysqli_connect("localhost","root","","db_simpeg"); 
+// $conn = mysqli_connect($server, $user, $passw, $db);
+?>
