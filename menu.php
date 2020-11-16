fbcbf19eafd168060d51c991daaf7c238b9a0d48
herry081288 <herry081288@gmail.com>
2020-11-16 14:14:32 +0700

    updateMenuDone

--- menu.php
+++ menu.php
@@ -8,6 +8,15 @@
         while($a = mysqli_fetch_array($hasil)){
             echo "<li><a class='nav-link' href='$a[link]'><i class=\"far fa-home\"></i> <span>$a[nama_modul]</span></a></li>";
         }
+    } 
+    //jika menu aksesnya user
+    elseif($_SESSION['leveluser'] == 'user'){
+        $query = "SELECT * FROM modul WHERE aktif = 'Y' AND status = 'user' ORDER BY urutan";
+        $hasil = mysqli_query($conn, $query);
+        while($a = mysqli_fetch_array($hasil)){
+            echo "<li><a class='nav-link' href='$a[link]'><i class=\"far fa-home\"></i> <span>$a[nama_modul]</span></a></li>";
+        }
+
     }
 
 ?>
