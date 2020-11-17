5a136233fc3595ed2122d58a7477eece10da743d
herry081288 <herry081288@gmail.com>
2020-11-17 10:45:14 +0700

    SeginiDuluNantiDIlanjutijn

--- modul/mod_modul/aksi_modul.php
+++ modul/mod_modul/aksi_modul.php
@@ -4,6 +4,18 @@ if(empty($_SESSION['namauser']) AND empty($_SESSION['passuser'])){
     echo "<h1>Gagal Login</h1><br><br>";
     echo "<a href='index.php'>Ulangi</a>";
 } else{
+    include "../../config/connect.php";
+    $module = $_GET['module'];
+    $act = $_GET['act'];
+
+    //input data modul function
+    if($module =='modul' AND $act =='input'){
+        //cari urutan terakhir
+        $query = mysqli_query($conn, "SELECT urutan FROM modul ORDER BY urutan DESC LIMIT 1");
+        $r = mysqli_fetch_array($query);
+
+        
+    }
 
 }
 ?>
